18313d2c50860a69da645dae881a3ec69dc47b42
roshank8848 <topofeverest8848@gmail.com>
2025-02-28 12:03:56 +0545

    feat: add jwt authentication and token validation
    Added jwt authetication on some endpoints for testing and implemented
    jwt token validation based on jwks uri. Token validation aids in the
    authenticity of the token and protects against token forgery.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.database import engine, Base
 from app.routers import users_router, todos_router
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+from app.auth.jwtvalidation import get_current_user
+from app.schemas.tokendata import TokenData
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 Base.metadata.create_all(bind=engine)
 
@@ -19,6 +25,11 @@ app.add_middleware(
 
 app.include_router(users_router)
 app.include_router(todos_router)
+
+
+@app.get("/secure")
+async def secure_endpoint(current_user: TokenData = Depends(get_current_user)):
+    return {"message": "This is a secure endpoint", "current_user": current_user}
 
 
 @app.get("/")
