ac85c7662e1c534da88c55fc86fe72979075fde5
roshank8848 <topofeverest8848@gmail.com>
2025-08-06 21:07:21 +0545

    fix: display name and preferred_username on decoded token

--- app/auth/jwtvalidation.py
+++ app/auth/jwtvalidation.py
@@ -57,9 +57,10 @@ def verify_token(token: str):
         logger.debug(f"payload: {payload}")
         roles = []
         resource_access = payload.get("resource_access", {})
-        for resource in resource_access.values():
-            roles.extend(resource.get("roles", []))
-        return TokenData(sub=payload["sub"], email=payload["email"], roles=roles)
+        for resource_name, resource in resource_access.items():
+            if resource_name == "springboot":
+                roles.extend(resource.get("roles", []))
+        return TokenData(name=payload["name"], preferred_username=payload["preferred_username"], sub=payload["sub"], email=payload["email"], roles=roles)
     except jwt.PyJWTError as e:
         logger.error(f"PyJWTError: {e}")
         raise HTTPException(status_code=401, detail="Could not validate credentials")
